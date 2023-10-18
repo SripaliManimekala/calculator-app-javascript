8ae016d84be9415f8be5db098e682050b5210c19
SripaliManimekala <sripalimanimekala@gmail.com>
2023-10-19 00:59:53 +0530

    handling of click events and operatios

---
+++ script.js
@@ -0,0 +1,87 @@
+const currDisplay = document.querySelector(".curr-display");
+const prevDisplay = document.querySelector(".prev-display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operation");
+const clearBtn = document.querySelector(".clear");
+const delBtn = document.querySelector(".delete");
+const equalBtn = document.querySelector(".equal");
+let operation;
+
+function appendNumber(number) {
+  if (number === "." && currDisplay.innerText.includes(".")) return;
+  currDisplay.innerText += number;
+}
+
+function chooseOperation(operand) {
+  if (currDisplay.innerText === "") return;
+  compute(operand);
+  operation = operand;
+  currDisplay.innerText += operand;
+  prevDisplay.innerText = currDisplay.innerText;
+  currDisplay.innerText = "";
+}
+
+function clearDisplay() {
+  currDisplay.innerText = "";
+  prevDisplay.innerText = "";
+}
+
+function compute(operand) {
+  let result;
+  const previousValue = parseFloat(prevDisplay.innerText);
+  const currentValue = parseFloat(currDisplay.innerText);
+
+  if (isNaN(previousValue) || isNaN(currentValue)) return;
+
+  switch (operation) {
+    case "+":
+      result = previousValue + currentValue;
+      break;
+    case "-":
+      result = previousValue - currentValue;
+      break;
+    case "*":
+      result = previousValue * currentValue;
+      break;
+    case "/":
+      result = previousValue / currentValue;
+      break;
+    case "%":
+      result = previousValue / 100;
+      break;
+    case "sqrt":
+      result = previousValue * previousValue;
+      break;
+    case "sqr":
+      result = previousValue * previousValue;
+      break;
+    default:
+      return;
+  }
+  currDisplay.innerText = result;
+}
+
+numbers.forEach((number) => {
+  number.addEventListener("click", () => {
+    appendNumber(number.innerText);
+  });
+});
+
+operators.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    chooseOperation(operand.innerText);
+  });
+});
+
+clearBtn.addEventListener("click", () => {
+  clearDisplay();
+});
+
+equalBtn.addEventListener("click", () => {
+  compute();
+  prevDisplay.innerText = "";
+});
+
+delBtn.addEventListener("click", () => {
+  currDisplay.innerText = currDisplay.innerText.slice(0, -1);
+});
